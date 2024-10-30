b6ec9ce2c20da9397f98ac28c69d67b85cefdae0
dorotaphanSiili <108282075+dorotaphanSiili@users.noreply.github.com>
2024-10-30 10:24:24 +0100

    feat(nats): add nats base
    This is base class for client and service as they have lot of common
    functionality.
    This is part of #24 task.

--- templates/apigear/nats/natsservice.h
+++ templates/apigear/nats/natsservice.h
@@ -2,24 +2,17 @@
 
 #include <memory>
 #include "natscommon.h"
+#include "natsbase.h"
 #include <string>
 
 namespace ApiGear {
 namespace Nats {
 
-class APIGEAR_NATS_EXPORT Service
+class APIGEAR_NATS_EXPORT Service : public Base
 {
 public:
     explicit Service();
     virtual ~Service() = default;
-
-    void connect(std::string address);
-    void subscribe(std::string topic);
-    void unsubscribe(std::string topic);
-    void publish(std::string topic, std::string payload);
-
-private:
-    std::shared_ptr<class CWrapper> m_cwrapper;
 };
 } // namespace Nats
 } // namespace ApiGear
