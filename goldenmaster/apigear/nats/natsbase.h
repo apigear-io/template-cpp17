c7e69602df128d0f58a8f37f839679bec40a705a
dorotaphanSiili <108282075+dorotaphanSiili@users.noreply.github.com>
2024-10-30 10:24:24 +0100

    feat(nats): add nats base
    This is base class for client and service as they have lot of common
    functionality.
    This is part of #24 task.

---
+++ goldenmaster/apigear/nats/natsbase.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include "natscommon.h"
+#include "natstypes.h"
+
+namespace ApiGear {
+namespace Nats {
+
+class APIGEAR_NATS_EXPORT Base
+{
+public:
+    explicit Base();
+    virtual ~Base() = default;
+
+    void connect(std::string address);
+    void subscribe(std::string topic);
+    void unsubscribe(std::string topic);
+    void publish(std::string topic, std::string payload);
+
+private:
+    std::shared_ptr<class CWrapper> m_cwrapper;
+};
+} // namespace Nats
+} // namespace ApiGear
--- templates/apigear/nats/natsservice.cpp
+++ templates/apigear/nats/natsservice.cpp
@@ -5,22 +5,4 @@ using namespace ApiGear::Nats;
 
 Service::Service()
 {
-    m_cwrapper = CWrapper::create();
-}
-
-void Service::connect(std::string address)
-{
-    m_cwrapper->connect(address);
-}
-void Service::subscribe(std::string topic)
-{
-    m_cwrapper->subscribe(topic);
-}
-void Service::unsubscribe(std::string topic)
-{
-    m_cwrapper->unsubscribe(topic);
-}
-void Service::publish(std::string topic, std::string payload)
-{
-    m_cwrapper->publish(topic, payload);
 }
