653cf82fbcb09160be5d1d195afc78ae121cec80
dorotaphanSiili <108282075+dorotaphanSiili@users.noreply.github.com>
2024-10-30 10:24:24 +0100

    feat(nats): add nats base
    This is base class for client and service as they have lot of common
    functionality.
    This is part of #24 task.

--- templates/apigear/nats/natsclient.cpp
+++ templates/apigear/nats/natsclient.cpp
@@ -5,22 +5,4 @@ using namespace ApiGear::Nats;
 
 Client::Client()
 {
-    m_cwrapper = CWrapper::create();
-}
-
-void Client::connect(std::string address)
-{
-    m_cwrapper->connect(address);
-}
-void Client::subscribe(std::string topic)
-{
-    m_cwrapper->subscribe(topic);
-}
-void Client::unsubscribe(std::string topic)
-{
-    m_cwrapper->unsubscribe(topic);
-}
-void Client::publish(std::string topic, std::string payload)
-{
-    m_cwrapper->publish(topic, payload);
 }
