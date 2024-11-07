5e4c02fedd4f670ae91f2de24d7aeab956a20b96
dorotaphanSiili <108282075+dorotaphanSiili@users.noreply.github.com>
2024-11-07 10:32:28 +0100

    feat(nats): handle request-response
    Prepare the support to use request-response nats mechanism.
    It will be necessary for handling methods remote calls.
    The functionality added covers subbscribe, unsubscribe, storing callbacks,
    Also disconnect added.
    The implementation uses similar structures and functions as for subscriptions,
    with details different - the callback added for topic has a return value.
    In contrary to publish subscribe mechanism which is to be used for properties and signals,
    only the requestor gets the response, not all the client adapters for same interface.
    The request-response mechanism got its own thread pool in nats base.
    
    This is part of #24 task.

--- templates/apigear/nats/natsbase.h
+++ templates/apigear/nats/natsbase.h
@@ -7,22 +7,33 @@
 #include <mutex>
 #include <atomic>
 #include <future>
-#include "apigear/utilities/threadpool.h"
 
 namespace ApiGear {
+
+namespace Utilities {
+    class ThreadPool;
+}
+
 namespace Nats {
+
 
 class APIGEAR_NATS_EXPORT Base
 {
 public:
+
     explicit Base();
-    virtual ~Base() = default;
+    virtual ~Base();
+
+    static const int64_t InvalidSubscriptionId = 0;
 
     // WARNING this is blocking call, but calling with std::async causes error with state NATS_NO_SERVER_SUPPORT 
     void connect(const std::string& address);
+    void disconnect();
     void subscribe(const std::string& topic, SimpleOnMessageCallback callback, std::function<void(int64_t, std::string, bool)> subscribe_callback);
+    void subscribeForRequest(const std::string& topic, MessageCallbackWithResult callback, std::function<void(int64_t, std::string, bool)> subscribe_callback);
     void unsubscribe(int64_t id);
     void publish(const std::string& topic, const std::string& payload);
+    void request(const std::string& topic, const std::string& payload, SimpleOnMessageCallback responseHandler);
 
     uint32_t addOnConnectedCallback(OnConnectionStatusChangedCallBackFunction callback);
     void removeOnConnectedCallback(uint32_t id);
@@ -35,8 +46,9 @@ private:
 
     std::shared_ptr<class CWrapper> m_cwrapper;
 
-    /** subscriptions pool*/
+    /* thread pools for c_wrapper thread blocking events*/
     std::unique_ptr<ApiGear::Utilities::ThreadPool> m_subscriptions_pool;
+    std::unique_ptr<ApiGear::Utilities::ThreadPool> m_requests_pool;
 };
 } // namespace Nats
 } // namespace ApiGear
