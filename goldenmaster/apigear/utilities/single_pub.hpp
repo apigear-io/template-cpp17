e7d8dd4d59962477a4213bfabe1e59078a657a8d
dorotaphanSiili <108282075+dorotaphanSiili@users.noreply.github.com>
2024-11-14 15:39:16 +0100

    feat: add publisher helper
    Add a templated class as a publisher helper.
    Its puropse is to help keep track of subscribers:
    it allows subscribing and provide an id to unsubscribe, on publishChange
    call it notifies all the subscribers about the change.
    It is templated to allow different types of the changed value, and to
    publish signals with many parameters.
    It does not store any value.
    It is thread safe.
    The id generating mechanism may be improved.
    
    It will be used in next commit to keep track of the nats adapters state.
    It also may be used for generated interface publishers.
    
    This is part of #24 task.

---
+++ goldenmaster/apigear/utilities/single_pub.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <atomic>
+#include <map>
+#include <shared_mutex>
+
+namespace ApiGear {
+namespace Utilities {
+
+template<typename... Arguments>
+class SinglePub
+{
+public:
+    unsigned long subscribeForChange(std::function<void(Arguments...)> callback)
+    {
+        // this is a short term workaround - we need a better solution for unique handle identifiers
+        auto handleId = m_nextId++;
+        std::unique_lock<std::shared_timed_mutex> lock(m_callbacksMutex);
+        m_callbacks[handleId] = callback;
+        return handleId;
+    }
+    void unsubscribeFromChange(unsigned long handleId)
+    {
+        std::unique_lock<std::shared_timed_mutex> lock(m_callbacksMutex);
+        m_callbacks.erase(handleId);
+    }
+    void publishChange(Arguments... params)
+    {
+        std::shared_lock<std::shared_timed_mutex> lock(m_callbacksMutex);
+        const auto callbacks = m_callbacks;
+        lock.unlock();
+        for (const auto& callbackEntry : callbacks)
+        {
+            if (callbackEntry.second)
+            {
+                callbackEntry.second(params...);
+            }
+        }
+    }
+private:
+    std::atomic<unsigned long> m_nextId{ 0 };
+    std::map<unsigned long, std::function<void(Arguments...)> > m_callbacks;
+    std::shared_timed_mutex m_callbacksMutex;
+
+};
+}} //namespace ApiGear::Utilities
