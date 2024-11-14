93a38c128f6a955a180d27db1c5878831025e7ac
dorotaphanSiili <108282075+dorotaphanSiili@users.noreply.github.com>
2024-11-14 15:40:19 +0100

    feat(nats): add base adapter
    This commit introduces a base class for the generated adapters,
    both client and service side.
    The functionality it covers:
    handling is ready for an adapter- where is ready means that the adapter
    has working connection and has sucessfully subscribed to all topics it needed.
    It also keeps track of the subscription state.
    
    This is part of th #24 task.

---
+++ templates/apigear/nats/baseadapter.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "apigear/nats/natsbase.h"
+#include "apigear/nats/natstypes.h"
+#include "apigear/utilities/single_pub.hpp"
+
+#include <future>
+#include <functional>
+#include <unordered_map>
+#include <string>
+#include <memory>
+
+namespace ApiGear {
+namespace Nats {
+class APIGEAR_NATS_EXPORT BaseAdapter
+{
+public:
+    explicit BaseAdapter(std::shared_ptr<Base> client);
+    virtual ~BaseAdapter();
+
+    unsigned long _subscribeForIsReady(std::function<void(bool)> sub_function);
+    void _unsubscribeFromIsReady(unsigned long id);
+    bool _is_ready();
+
+protected:
+    void subscribeTopic(const std::string& topic, SimpleOnMessageCallback callback);
+    void subscribeRequest(const std::string& topic, MessageCallbackWithResult callback);
+    void init(std::function<void(void)> onConnectedCallback);
+
+private:
+    std::shared_ptr<Base> m_client;
+    void unsubscribeTopics();
+    void onSubscribed(int64_t id, const std::string& topic, bool is_subscribed);
+    bool isAlreadyAdded(const std::string& topic);
+  
+    ApiGear::Utilities::SinglePub<bool> _is_readyChanges;
+    struct SubscriptionInfo
+    {
+        SubscriptionInfo(){};
+        SubscriptionInfo(ApiGear::Nats::SubscriptionStatus in_status)
+        :status(in_status)
+        {
+        }
+        SubscriptionInfo(ApiGear::Nats::SubscriptionStatus in_status, int64_t in_id)
+        :status(in_status),
+        id(in_id)
+        {
+        }
+        ApiGear::Nats::SubscriptionStatus status = ApiGear::Nats::SubscriptionStatus::unsubscribed;
+        int64_t id = ApiGear::Nats::Base::InvalidSubscriptionId;
+    };
+    std::unordered_map<std::string, SubscriptionInfo> m_subscribedTopics;
+};
+} // namespace Nats
+} // namespace ApiGear
