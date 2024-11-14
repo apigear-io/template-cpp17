987596301f0e5f769acb8d227f38ac47ac691f2d
dorotaphanSiili <108282075+dorotaphanSiili@users.noreply.github.com>
2024-11-14 15:40:19 +0100

    feat(nats): add base adapter
    This commit introduces a base class for the generated adapters,
    both client and service side.
    The functionality it covers:
    handling is ready for an adapter- where is ready means that the adapter
    has working connection and has sucessfully subscribed to all topics it needed.
    It also keeps track of the subscription state.
    
    This is part of th #24 task.

---
+++ goldenmaster/apigear/nats/baseadapter.cpp
@@ -0,0 +1,147 @@
+#include "apigear/nats/baseadapter.h"
+#include "apigear/utilities/logger.h"
+
+using namespace ApiGear::Nats;
+
+BaseAdapter::BaseAdapter(std::shared_ptr<Base> client)
+    : m_client(client)
+{
+}
+
+void BaseAdapter::init(std::function<void(void)> onConnectedCallback)
+{
+    if (m_client->isConnected())
+    {
+        onConnectedCallback();
+    }
+    else
+    {
+        m_client->addOnConnectedCallback([this, onConnectedCallback](bool is_connected)
+            {
+                if (is_connected)
+                {
+                    onConnectedCallback();
+                    if (_is_ready())
+                    {
+                        _is_readyChanges.publishChange(true);
+                    }
+                }
+                else
+                {
+                    _is_readyChanges.publishChange(false);
+                }
+            });
+    }
+}
+
+BaseAdapter::~BaseAdapter()
+{
+    unsubscribeTopics();
+}
+
+void BaseAdapter::subscribeTopic(const std::string& topic, SimpleOnMessageCallback callback)
+{
+    // TODO take care of the object lifetime.
+    auto safeOnSubscribed = [this](int64_t id, const std::string& topic, bool is_subscribed)
+        {
+            onSubscribed(id, topic, is_subscribed);
+        };
+    if (!isAlreadyAdded(topic))
+    {
+        m_client->subscribe(topic, callback, safeOnSubscribed);
+    }
+}
+
+void BaseAdapter::subscribeRequest(const std::string& topic, MessageCallbackWithResult callback)
+{
+    // TODO take care of the object lifetime.
+    auto safeOnSubscribed = [this](int64_t id, const std::string& topic, bool is_subscribed)
+        {
+            onSubscribed(id, topic, is_subscribed);
+        };
+    if (!isAlreadyAdded(topic))
+    {
+        m_client->subscribeForRequest(topic, callback, safeOnSubscribed);
+    }
+}
+
+//TODO mutex
+bool BaseAdapter::isAlreadyAdded(const std::string& topic)
+{
+    auto already_added = m_subscribedTopics.find(topic);
+    return already_added != m_subscribedTopics.end() &&
+        (already_added->second.status == ApiGear::Nats::SubscriptionStatus::subscribed ||
+            already_added->second.status == ApiGear::Nats::SubscriptionStatus::subscribing);
+}
+
+void BaseAdapter::unsubscribeTopics()
+{
+    for (auto topic : m_subscribedTopics)
+    {
+        if (topic.second.status == ApiGear::Nats::SubscriptionStatus::subscribed)
+        {
+            topic.second.status = ApiGear::Nats::SubscriptionStatus::unsubscribing;
+            m_client->unsubscribe(topic.second.id);
+        }
+        else if (topic.second.status != ApiGear::Nats::SubscriptionStatus::subscribing)
+        {
+            topic.second.status = ApiGear::Nats::SubscriptionStatus::to_unsubscribe;
+        }
+    }
+}
+
+void BaseAdapter::onSubscribed(int64_t id, const std::string& topic, bool is_subscribed)
+{
+    auto subscription = m_subscribedTopics.find(topic);
+    if (is_subscribed)
+    {
+        if (subscription != m_subscribedTopics.end() && subscription->second.status == ApiGear::Nats::SubscriptionStatus::to_unsubscribe)
+        {
+            m_client->unsubscribe(id);
+            m_subscribedTopics[topic] = SubscriptionInfo(ApiGear::Nats::SubscriptionStatus::unsubscribing);
+        }
+        else
+        {
+            m_subscribedTopics[topic] = SubscriptionInfo(ApiGear::Nats::SubscriptionStatus::subscribed, id);
+        }
+    }
+    else
+    {
+        if (subscription != m_subscribedTopics.end())
+        {
+            subscription->second.status = ApiGear::Nats::SubscriptionStatus::unsubscribed;
+        }
+        AG_LOG_ERROR("failed to subscribe " + topic);
+    }
+    auto still_not_all_subscribed = std::find_if(m_subscribedTopics.begin(), m_subscribedTopics.end(),
+        [](auto& element) {return element.second.status != ApiGear::Nats::SubscriptionStatus::subscribed; })
+        != m_subscribedTopics.end();
+    if (!still_not_all_subscribed)
+    {
+        if (_is_ready())
+        {
+            _is_readyChanges.publishChange(true);
+        }
+    }
+
+}
+
+unsigned long BaseAdapter::_subscribeForIsReady(std::function<void(bool)> sub_function)
+{
+    return _is_readyChanges.subscribeForChange(sub_function);
+}
+
+void BaseAdapter::_unsubscribeFromIsReady(unsigned long id)
+{
+    _is_readyChanges.unsubscribeFromChange(id);
+}
+
+bool BaseAdapter::_is_ready()
+{
+    auto still_not_all_subscribed = std::find_if(m_subscribedTopics.cbegin(),
+        m_subscribedTopics.cend(),
+        [](const auto& element) {return element.second.status != ApiGear::Nats::SubscriptionStatus::subscribed; })
+        != m_subscribedTopics.end();
+    return m_client->isConnected() &&
+        !still_not_all_subscribed;
+}
