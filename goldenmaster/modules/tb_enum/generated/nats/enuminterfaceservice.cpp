244adebfd6e3d5dffe3ec72d0b287f41b20486b5
dorotaphanSiili <108282075+dorotaphanSiili@users.noreply.github.com>
2024-11-18 11:09:28 +0100

    feat(nats): nats service adapters
    Service Interface adapters now are using nats protocol.
    They us base adatapter as a base class.
    As the base requires weak_ptr it inherites enabl_shared_from_this.
    The weak_ptr is necessary for making sure that callbacks passed to nats
    client are always safe to call especially during tear down.
    This is also why it has a create method instead of public constructor,
    and it has an init method - to which all the logic for initialization
    was moved to ensure no virtual method will be called on construction.

--- goldenmaster/modules/tb_enum/generated/nats/enuminterfaceservice.cpp
+++ goldenmaster/modules/tb_enum/generated/nats/enuminterfaceservice.cpp
@@ -5,12 +5,37 @@
 using namespace Test::TbEnum;
 using namespace Test::TbEnum::Nats;
 
+namespace{
+const uint32_t  expectedMethodSubscriptions = 4;
+const uint32_t  expectedPropertiesSubscriptions = 4;
+constexpr uint32_t expectedSubscriptionsCount = expectedMethodSubscriptions + expectedPropertiesSubscriptions;
+}
+
 EnumInterfaceService::EnumInterfaceService(std::shared_ptr<IEnumInterface> impl, std::shared_ptr<ApiGear::Nats::Service> service)
-    : m_impl(impl)
+    :BaseAdapter(service, expectedSubscriptionsCount)
+    , m_impl(impl)
     , m_service(service)
 {
     m_impl->_getPublisher().subscribeToAllChanges(*this);
 }
+
+void EnumInterfaceService::init()
+{
+    BaseAdapter::init([this](){onConnected();});
+}
+
+std::shared_ptr<EnumInterfaceService> EnumInterfaceService::create(std::shared_ptr<IEnumInterface> impl, std::shared_ptr<ApiGear::Nats::Service> service)
+{
+    std::shared_ptr<EnumInterfaceService> obj(new EnumInterfaceService(impl, service));
+    obj->init();
+    return obj;
+}
+
+std::shared_ptr<ApiGear::Nats::BaseAdapter> EnumInterfaceService::getSharedFromDerrived()
+{
+    return shared_from_this();
+}
+
 
 EnumInterfaceService::~EnumInterfaceService()
 {
@@ -18,13 +43,16 @@ EnumInterfaceService::~EnumInterfaceService()
 }
 
 
-void EnumInterfaceService::onConnectionStatusChanged(bool connectionStatus)
+void EnumInterfaceService::onConnected()
 {
-    if(!connectionStatus)
-    {
-        return;
-    }
-    // TODO send current values through service
+    subscribeTopic("tb.enum.EnumInterface.set.prop0", [this](const auto& value){ onSetProp0(value); });
+    subscribeTopic("tb.enum.EnumInterface.set.prop1", [this](const auto& value){ onSetProp1(value); });
+    subscribeTopic("tb.enum.EnumInterface.set.prop2", [this](const auto& value){ onSetProp2(value); });
+    subscribeTopic("tb.enum.EnumInterface.set.prop3", [this](const auto& value){ onSetProp3(value); });
+    subscribeRequest("tb.enum.EnumInterface.rpc.func0", [this](const auto& args){  return onInvokeFunc0(args); });
+    subscribeRequest("tb.enum.EnumInterface.rpc.func1", [this](const auto& args){  return onInvokeFunc1(args); });
+    subscribeRequest("tb.enum.EnumInterface.rpc.func2", [this](const auto& args){  return onInvokeFunc2(args); });
+    subscribeRequest("tb.enum.EnumInterface.rpc.func3", [this](const auto& args){  return onInvokeFunc3(args); });
 }
 void EnumInterfaceService::onSetProp0(const std::string& args) const
 {
@@ -73,40 +101,76 @@ void EnumInterfaceService::onSetProp3(const std::string& args) const
 void EnumInterfaceService::onSig0(Enum0Enum param0)
 {
     (void) param0;
-//TODO use service to notify clients
+    static const std::string topic = "tb.enum.EnumInterface.sig.sig0";
+    nlohmann::json args = { param0 };
+    m_service->publish(topic, nlohmann::json(args).dump());
 }
 void EnumInterfaceService::onSig1(Enum1Enum param1)
 {
     (void) param1;
-//TODO use service to notify clients
+    static const std::string topic = "tb.enum.EnumInterface.sig.sig1";
+    nlohmann::json args = { param1 };
+    m_service->publish(topic, nlohmann::json(args).dump());
 }
 void EnumInterfaceService::onSig2(Enum2Enum param2)
 {
     (void) param2;
-//TODO use service to notify clients
+    static const std::string topic = "tb.enum.EnumInterface.sig.sig2";
+    nlohmann::json args = { param2 };
+    m_service->publish(topic, nlohmann::json(args).dump());
 }
 void EnumInterfaceService::onSig3(Enum3Enum param3)
 {
     (void) param3;
-//TODO use service to notify clients
+    static const std::string topic = "tb.enum.EnumInterface.sig.sig3";
+    nlohmann::json args = { param3 };
+    m_service->publish(topic, nlohmann::json(args).dump());
 }
 void EnumInterfaceService::onProp0Changed(Enum0Enum prop0)
 {
-    (void)prop0;
-    //TODO use service to notify clients
+    static const std::string topic = "tb.enum.EnumInterface.prop.prop0";
+    m_service->publish(topic, nlohmann::json(prop0).dump());
 }
 void EnumInterfaceService::onProp1Changed(Enum1Enum prop1)
 {
-    (void)prop1;
-    //TODO use service to notify clients
+    static const std::string topic = "tb.enum.EnumInterface.prop.prop1";
+    m_service->publish(topic, nlohmann::json(prop1).dump());
 }
 void EnumInterfaceService::onProp2Changed(Enum2Enum prop2)
 {
-    (void)prop2;
-    //TODO use service to notify clients
+    static const std::string topic = "tb.enum.EnumInterface.prop.prop2";
+    m_service->publish(topic, nlohmann::json(prop2).dump());
 }
 void EnumInterfaceService::onProp3Changed(Enum3Enum prop3)
 {
-    (void)prop3;
-    //TODO use service to notify clients
+    static const std::string topic = "tb.enum.EnumInterface.prop.prop3";
+    m_service->publish(topic, nlohmann::json(prop3).dump());
+}
+std::string EnumInterfaceService::onInvokeFunc0(const std::string& args) const
+{
+    nlohmann::json json_args = nlohmann::json::parse(args);
+    const Enum0Enum& param0 = json_args.at(0).get<Enum0Enum>();
+    auto result = m_impl->func0(param0);
+    return nlohmann::json(result).dump();
+}
+std::string EnumInterfaceService::onInvokeFunc1(const std::string& args) const
+{
+    nlohmann::json json_args = nlohmann::json::parse(args);
+    const Enum1Enum& param1 = json_args.at(0).get<Enum1Enum>();
+    auto result = m_impl->func1(param1);
+    return nlohmann::json(result).dump();
+}
+std::string EnumInterfaceService::onInvokeFunc2(const std::string& args) const
+{
+    nlohmann::json json_args = nlohmann::json::parse(args);
+    const Enum2Enum& param2 = json_args.at(0).get<Enum2Enum>();
+    auto result = m_impl->func2(param2);
+    return nlohmann::json(result).dump();
+}
+std::string EnumInterfaceService::onInvokeFunc3(const std::string& args) const
+{
+    nlohmann::json json_args = nlohmann::json::parse(args);
+    const Enum3Enum& param3 = json_args.at(0).get<Enum3Enum>();
+    auto result = m_impl->func3(param3);
+    return nlohmann::json(result).dump();
 }
