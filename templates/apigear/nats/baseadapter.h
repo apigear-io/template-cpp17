b591e8d4bab58850d008bc8d37a7c14453c926e3
dorotaphanSiili <108282075+dorotaphanSiili@users.noreply.github.com>
2024-11-21 14:02:18 +0100

    feat(nats): add thread safety to base adapter
    intorduces mutexes for stored subscriptions,
    ensures that the callbacks won't be used after the object deleted
    fixes checking for is ready:
    in case the subscriptions are added one by one after the connection was
    established the is connected callback was called after each subscription
    added (the vector size didn't had its full size yet). Now it is ensured
    that the isReady will actually wait for all the subscriptions.

--- templates/apigear/nats/baseadapter.h
+++ templates/apigear/nats/baseadapter.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "apigear/nats/natsbase.h"
-#include "apigear/nats/natstypes.h"
+#include "natsbase.h"
+#include "natstypes.h"
 #include "apigear/utilities/single_pub.hpp"
 
 #include <future>
@@ -9,20 +9,22 @@
 #include <unordered_map>
 #include <string>
 #include <memory>
+#include <mutex>
 
 namespace ApiGear {
 namespace Nats {
 class APIGEAR_NATS_EXPORT BaseAdapter
 {
 public:
-    explicit BaseAdapter(std::shared_ptr<Base> client);
     virtual ~BaseAdapter();
 
     unsigned long _subscribeForIsReady(std::function<void(bool)> sub_function);
     void _unsubscribeFromIsReady(unsigned long id);
-    bool _is_ready();
+    bool _is_ready() const;
 
 protected:
+    explicit BaseAdapter(std::shared_ptr<Base> client, uint32_t expectedSubscriptionCount);
+    virtual std::shared_ptr<BaseAdapter> getSharedFromDerrived() = 0;
     void subscribeTopic(const std::string& topic, SimpleOnMessageCallback callback);
     void subscribeRequest(const std::string& topic, MessageCallbackWithResult callback);
     void init(std::function<void(void)> onConnectedCallback);
@@ -49,7 +51,10 @@ private:
         ApiGear::Nats::SubscriptionStatus status = ApiGear::Nats::SubscriptionStatus::unsubscribed;
         int64_t id = ApiGear::Nats::Base::InvalidSubscriptionId;
     };
+
+    std::mutex m_subscribedTopicsMutex;
     std::unordered_map<std::string, SubscriptionInfo> m_subscribedTopics;
+    const uint32_t m_expectedSubscriptionsCount;
 };
 } // namespace Nats
 } // namespace ApiGear
