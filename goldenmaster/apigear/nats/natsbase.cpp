40a622f35112ea1da39f6ee8c8a6d0c6dd7c3945
dorotaphanSiili <108282075+dorotaphanSiili@users.noreply.github.com>
2024-12-03 16:03:45 +0100

    feat(nats): handle connection in natsbase
    This is an interface for using cwrapper.
    Handles connection and minds the connection state.
    Allows adding callback for connection state changed.

--- goldenmaster/apigear/nats/natsbase.cpp
+++ goldenmaster/apigear/nats/natsbase.cpp
@@ -1,17 +1,80 @@
 #include "natsbase.h"
 #include "private/natscwrapper.h"
+#include "apigear/utilities/logger.h"
+#include <random>
 
 using namespace ApiGear::Nats;
+
+std::mt19937 randomNumberGenerator(std::random_device{}());
+std::uniform_int_distribution<uint32_t> distribution(0, 0xFFFFFFFF);
+
+template<typename StoredItem>
+uint32_t createUniqueMapId(const std::map<uint32_t, StoredItem>& existing_map, std::mutex& map_mutex)
+{
+    uint32_t uniqueId = 0;
+    std::unique_lock<std::mutex> lock(map_mutex);
+    do {
+        uniqueId = distribution(randomNumberGenerator);
+    } while (existing_map.find(uniqueId) != existing_map.end());
+
+    return uniqueId;
+}
+
 
 Base::Base()
 {
     m_cwrapper = CWrapper::create();
 }
 
-void Base::connect(std::string address)
+void Base::connect(const std::string& address)
 {
-    m_cwrapper->connect(address, [this]() { m_cwrapper->getStatus(); });
+    auto status = m_cwrapper->getStatus();
+    if (status == ConnectionStatus::reconnecting ||
+        status == ConnectionStatus::connected ||
+        status == ConnectionStatus::connecting)
+    {
+        return;
+    }
+    m_cwrapper->connect(address, [this]() { onConnectedChanged(); });
+    if (m_cwrapper->getStatus() == ConnectionStatus::connected)
+    {
+        AG_LOG_DEBUG("nats client connected");
+        handleConnectionState(true);
+    }
 }
+
+void Base::handleConnectionState(bool state)
+{
+    m_onConnectionStatusChangedCallbacksMutex.lock();
+    auto onConnectionStatusChangedCallbacks{ m_onConnectionStatusChangedCallbacks };
+    m_onConnectionStatusChangedCallbacksMutex.unlock();
+    for (auto& callback : onConnectionStatusChangedCallbacks)
+    {
+        callback.second(state);
+    }
+}
+
+bool Base::onConnectedChanged()
+{
+    auto status = m_cwrapper->getStatus();
+    if (status == ConnectionStatus::connected)
+    {
+        AG_LOG_DEBUG("nats client connected");
+        handleConnectionState(true);
+    }
+    else if (status == ConnectionStatus::disconnected || status == ConnectionStatus::closed)
+    {
+        AG_LOG_DEBUG("nats client disconnected");
+        handleConnectionState(false);
+    }
+    return false;
+}
+
+bool Base::isConnected() const
+{
+    return m_cwrapper->getStatus() == ConnectionStatus::connected;
+}
+
 void Base::subscribe(std::string topic)
 {
     m_cwrapper->subscribe(topic);
@@ -23,4 +86,24 @@ void Base::unsubscribe(std::string topic)
 void Base::publish(std::string topic, std::string payload)
 {
     m_cwrapper->publish(topic, payload);
+}
+
+uint32_t Base::addOnConnectedCallback(OnConnectionStatusChangedCallBackFunction callBack)
+{
+    auto callbackId = createUniqueMapId(m_onConnectionStatusChangedCallbacks, m_onConnectionStatusChangedCallbacksMutex);
+    std::unique_lock<std::mutex> lock(m_onConnectionStatusChangedCallbacksMutex);
+    m_onConnectionStatusChangedCallbacks[callbackId] = callBack;
+    lock.unlock();
+
+    return callbackId;
+}
+
+void Base::removeOnConnectedCallback(uint32_t callbackId)
+{
+    std::unique_lock<std::mutex> lock(m_onConnectionStatusChangedCallbacksMutex);
+    if ((m_onConnectionStatusChangedCallbacks.find(callbackId) != m_onConnectionStatusChangedCallbacks.end()))
+    {
+        m_onConnectionStatusChangedCallbacks.erase(callbackId);
+    }
+    lock.unlock();
 }
