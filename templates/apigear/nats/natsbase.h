a9e36929f2d803a69e3015c0a788e3ce4c615a50
dorotaphanSiili <108282075+dorotaphanSiili@users.noreply.github.com>
2024-12-03 16:03:45 +0100

    feat(nats): handle connection in natsbase
    This is an interface for using cwrapper.
    Handles connection and minds the connection state.
    Allows adding callback for connection state changed.

--- templates/apigear/nats/natsbase.h
+++ templates/apigear/nats/natsbase.h
@@ -4,6 +4,8 @@
 #include <string>
 #include "natscommon.h"
 #include "natstypes.h"
+#include <mutex>
+#include <condition_variable>
 
 namespace ApiGear {
 namespace Nats {
@@ -14,12 +16,20 @@ public:
     explicit Base();
     virtual ~Base() = default;
 
-    void connect(std::string address);
+    void connect(const std::string& address);
     void subscribe(std::string topic);
     void unsubscribe(std::string topic);
     void publish(std::string topic, std::string payload);
 
+    uint32_t addOnConnectedCallback(OnConnectionStatusChangedCallBackFunction callback);
+    void removeOnConnectedCallback(uint32_t id);
+    bool isConnected() const;
 private:
+    bool onConnectedChanged();
+    void handleConnectionState(bool state);
+    std::mutex m_onConnectionStatusChangedCallbacksMutex;
+    std::map<uint32_t, OnConnectionStatusChangedCallBackFunction> m_onConnectionStatusChangedCallbacks;
+
     std::shared_ptr<class CWrapper> m_cwrapper;
 };
 } // namespace Nats
