1239abe55ff86594b27271ae0c7ac69e3dfa95bd
dorotaphanSiili <108282075+dorotaphanSiili@users.noreply.github.com>
2025-01-09 08:29:32 +0100

    feat(mqtt): add generated tests
    For each test interface we generate the mqtt tests.
    Tests setup a client and a server and interface adapters.
    They do not provide a broker which has to be started before starting
    tests.
    The test are using non-default generated value for singals and
    properties.
    As results of methods cannot be predicted their result is not checked.
    They are just executed, and waited for result to be delivered.
    
    This is commit fo #3 task

---
+++ goldenmaster/modules/tb_names/generated/mqtt/tests/test_names.cpp
@@ -0,0 +1,246 @@
+#pragma warning (disable: 4251)
+#pragma warning (disable: 4099)
+
+#include <catch2/catch.hpp>
+#include <condition_variable>
+
+
+#include "tb_names/generated/core/test_struct_helper.h"
+#include "tb_names/implementation/names.h"
+#include "tb_names/generated/mqtt/namesclient.h"
+#include "tb_names/generated/mqtt/namesservice.h"
+
+
+#include "apigear/mqtt/mqttclient.h"
+#include "apigear/mqtt/mqttservice.h"
+
+// Those tests do not use network connection.
+// They are set in a way the client writes data straight into read function of server and vice versa.
+
+namespace{
+
+    int timeout = 2000;//in ms
+}
+
+using namespace Test;
+using namespace Test::TbNames;
+
+TEST_CASE("mqtt  tb.names NamEs tests")
+{
+    auto mqttservice = std::make_shared<ApiGear::MQTT::Service>("NamEstestServer");
+    auto mqttclient = std::make_shared<ApiGear::MQTT::Client>("NamEstestClient");
+
+    auto clientNamEs = std::make_shared<Test::TbNames::MQTT::Nam_EsClient>(mqttclient);
+    auto implNamEs= std::make_shared<Test::TbNames::NamEs>();
+    auto serviceNamEs = std::make_shared<Test::TbNames::MQTT::Nam_EsService>(implNamEs, mqttservice);
+
+    mqttservice->connectToHost("");
+    mqttclient->connectToHost("");
+
+    std::condition_variable m_wait;
+    std::mutex m_waitMutex;
+    std::unique_lock<std::mutex> lock(m_waitMutex, std::defer_lock);
+
+
+    std::atomic<bool> is_serviceConnected{ false };
+    auto service_connected_id = serviceNamEs->_subscribeForIsReady([&is_serviceConnected, &m_wait](auto connected)
+        {
+            if (connected)
+            {
+                is_serviceConnected = true;
+                m_wait.notify_all();
+            }
+        });
+    if (serviceNamEs->_is_ready() == true)
+    {
+        is_serviceConnected = true;
+        m_wait.notify_all();
+    }
+    lock.lock();
+    m_wait.wait_for(lock, std::chrono::milliseconds(timeout), [&is_serviceConnected]() { return is_serviceConnected == true; });
+    lock.unlock();
+    REQUIRE(is_serviceConnected);
+ 
+    std::atomic<bool> is_clientConnected{ false };
+    clientNamEs->_subscribeForIsReady([&is_clientConnected, &m_wait](auto connected)
+        {
+            if (connected)
+            {
+                is_clientConnected = true;
+                m_wait.notify_all();
+            }
+        });
+
+    lock.lock();
+    m_wait.wait_for(lock, std::chrono::milliseconds(timeout), [&is_clientConnected]() {return is_clientConnected  == true; });
+    lock.unlock();
+    REQUIRE(is_clientConnected);
+    SECTION("Test setting Switch")
+    {
+        std::atomic<bool> isSwitchChanged = false;
+        clientNamEs->_getPublisher().subscribeToSwitchChanged(
+        [&isSwitchChanged, &m_wait ](auto value){
+            isSwitchChanged  = true;
+            m_wait.notify_all();
+        });
+        auto test_value = true;
+        clientNamEs->setSwitch(test_value);;
+        lock.lock();
+        REQUIRE( m_wait.wait_for(lock, std::chrono::milliseconds(timeout), [&isSwitchChanged]() {return isSwitchChanged  == true; }));
+        lock.unlock();
+        REQUIRE(implNamEs->getSwitch() == test_value);
+        REQUIRE(clientNamEs->getSwitch() == test_value);
+    }
+    SECTION("Test setting SOME_PROPERTY")
+    {
+        std::atomic<bool> isSOME_PROPERTYChanged = false;
+        clientNamEs->_getPublisher().subscribeToSomePropertyChanged(
+        [&isSOME_PROPERTYChanged, &m_wait ](auto value){
+            isSOME_PROPERTYChanged  = true;
+            m_wait.notify_all();
+        });
+        auto test_value = 1;
+        clientNamEs->setSomeProperty(test_value);;
+        lock.lock();
+        REQUIRE( m_wait.wait_for(lock, std::chrono::milliseconds(timeout), [&isSOME_PROPERTYChanged]() {return isSOME_PROPERTYChanged  == true; }));
+        lock.unlock();
+        REQUIRE(implNamEs->getSomeProperty() == test_value);
+        REQUIRE(clientNamEs->getSomeProperty() == test_value);
+    }
+    SECTION("Test setting Some_Poperty2")
+    {
+        std::atomic<bool> isSome_Poperty2Changed = false;
+        clientNamEs->_getPublisher().subscribeToSomePoperty2Changed(
+        [&isSome_Poperty2Changed, &m_wait ](auto value){
+            isSome_Poperty2Changed  = true;
+            m_wait.notify_all();
+        });
+        auto test_value = 1;
+        clientNamEs->setSomePoperty2(test_value);;
+        lock.lock();
+        REQUIRE( m_wait.wait_for(lock, std::chrono::milliseconds(timeout), [&isSome_Poperty2Changed]() {return isSome_Poperty2Changed  == true; }));
+        lock.unlock();
+        REQUIRE(implNamEs->getSomePoperty2() == test_value);
+        REQUIRE(clientNamEs->getSomePoperty2() == test_value);
+    }
+    SECTION("Test setting enum_property")
+    {
+        std::atomic<bool> isenum_propertyChanged = false;
+        clientNamEs->_getPublisher().subscribeToEnumPropertyChanged(
+        [&isenum_propertyChanged, &m_wait ](auto value){
+            isenum_propertyChanged  = true;
+            m_wait.notify_all();
+        });
+        auto test_value = TbNames::Enum_With_Under_scoresEnum::second_value;
+        clientNamEs->setEnumProperty(test_value);;
+        lock.lock();
+        REQUIRE( m_wait.wait_for(lock, std::chrono::milliseconds(timeout), [&isenum_propertyChanged]() {return isenum_propertyChanged  == true; }));
+        lock.unlock();
+        REQUIRE(implNamEs->getEnumProperty() == test_value);
+        REQUIRE(clientNamEs->getEnumProperty() == test_value);
+    }
+    SECTION("Test emit SOME_SIGNAL")
+    {
+        std::atomic<bool> isSOME_SIGNALEmitted = false;
+
+        clientNamEs->_getPublisher().subscribeToSomeSignal(
+        [&m_wait, &isSOME_SIGNALEmitted](bool SOME_PARAM)
+        {
+            REQUIRE(SOME_PARAM == true);
+            isSOME_SIGNALEmitted  = true;
+            m_wait.notify_all();
+        });
+
+         implNamEs->_getPublisher().publishSomeSignal(true);
+        lock.lock();
+        REQUIRE( m_wait.wait_for(lock, std::chrono::milliseconds(timeout), [&isSOME_SIGNALEmitted ]() {return isSOME_SIGNALEmitted   == true; }));
+        lock.unlock();
+    }
+    SECTION("Test emit Some_Signal2")
+    {
+        std::atomic<bool> isSome_Signal2Emitted = false;
+
+        clientNamEs->_getPublisher().subscribeToSomeSignal2(
+        [&m_wait, &isSome_Signal2Emitted](bool Some_Param)
+        {
+            REQUIRE(Some_Param == true);
+            isSome_Signal2Emitted  = true;
+            m_wait.notify_all();
+        });
+
+         implNamEs->_getPublisher().publishSomeSignal2(true);
+        lock.lock();
+        REQUIRE( m_wait.wait_for(lock, std::chrono::milliseconds(timeout), [&isSome_Signal2Emitted ]() {return isSome_Signal2Emitted   == true; }));
+        lock.unlock();
+    }
+    SECTION("Test method SOME_FUNCTION")
+    {
+         clientNamEs->sOME_FUNCTION(false);
+        // CHECK EFFECTS OF YOUR METHOD AFER FUTURE IS DONE
+    }
+    SECTION("Test method SOME_FUNCTION async")
+    {
+        auto resultFuture = clientNamEs->sOME_FUNCTIONAsync(false);
+        // The void function only sends request. It does not wait for the actual function on server side to be finished.
+    }
+
+    SECTION("Test method SOME_FUNCTION async with callback")
+    {
+        auto resultFuture = clientNamEs->sOME_FUNCTIONAsync(false,[](){/* you can add a callback, but it will be called right after sending the request. It does not wait for the actual function on server side to be finished. */ });
+    }
+    SECTION("Test method Some_Function2")
+    {
+         clientNamEs->some_Function2(false);
+        // CHECK EFFECTS OF YOUR METHOD AFER FUTURE IS DONE
+    }
+    SECTION("Test method Some_Function2 async")
+    {
+        auto resultFuture = clientNamEs->some_Function2Async(false);
+        // The void function only sends request. It does not wait for the actual function on server side to be finished.
+    }
+
+    SECTION("Test method Some_Function2 async with callback")
+    {
+        auto resultFuture = clientNamEs->some_Function2Async(false,[](){/* you can add a callback, but it will be called right after sending the request. It does not wait for the actual function on server side to be finished. */ });
+    }
+
+    std::atomic<bool> serviceDisconnected{ false };
+    mqttservice->subscribeToConnectionStatus([&serviceDisconnected, &m_wait](auto isConnected) {
+        if (!isConnected)
+        {
+            serviceDisconnected = true;
+            m_wait.notify_all();
+        }
+        
+        });
+
+    mqttservice->disconnect();
+
+    lock.lock();
+    m_wait.wait_for(lock, std::chrono::milliseconds(timeout),
+        [&serviceDisconnected]() { return serviceDisconnected == true; });
+    lock.unlock();
+    REQUIRE(serviceDisconnected);
+
+    std::atomic<bool> clientDisonnected{ false };
+    mqttclient->subscribeToConnectionStatus([&clientDisonnected, &m_wait](auto isConnected) {
+        if (!isConnected)
+        {
+            clientDisonnected = true;
+            m_wait.notify_all();
+        }
+        });
+
+    mqttclient->disconnect();
+
+    lock.lock();
+    m_wait.wait_for(lock, std::chrono::milliseconds(timeout),
+        [&clientDisonnected]() { return clientDisonnected == true; });
+    lock.unlock();
+    REQUIRE(clientDisonnected);
+
+    mqttservice.reset();
+    mqttclient.reset();
+    serviceNamEs.reset();
+    clientNamEs.reset();
+}
---
+++ goldenmaster/modules/testbed2/generated/mqtt/tests/test_nestedstruct3interface.cpp
@@ -0,0 +1,322 @@
+#pragma warning (disable: 4251)
+#pragma warning (disable: 4099)
+
+#include <catch2/catch.hpp>
+#include <condition_variable>
+
+
+#include "testbed2/generated/core/test_struct_helper.h"
+#include "testbed2/implementation/nestedstruct3interface.h"
+#include "testbed2/generated/mqtt/nestedstruct3interfaceclient.h"
+#include "testbed2/generated/mqtt/nestedstruct3interfaceservice.h"
+
+
+#include "apigear/mqtt/mqttclient.h"
+#include "apigear/mqtt/mqttservice.h"
+
+// Those tests do not use network connection.
+// They are set in a way the client writes data straight into read function of server and vice versa.
+
+namespace{
+
+    int timeout = 2000;//in ms
+}
+
+using namespace Test;
+using namespace Test::Testbed2;
+
+TEST_CASE("mqtt  testbed2 NestedStruct3Interface tests")
+{
+    auto mqttservice = std::make_shared<ApiGear::MQTT::Service>("NestedStruct3InterfacetestServer");
+    auto mqttclient = std::make_shared<ApiGear::MQTT::Client>("NestedStruct3InterfacetestClient");
+
+    auto clientNestedStruct3Interface = std::make_shared<Test::Testbed2::MQTT::NestedStruct3InterfaceClient>(mqttclient);
+    auto implNestedStruct3Interface= std::make_shared<Test::Testbed2::NestedStruct3Interface>();
+    auto serviceNestedStruct3Interface = std::make_shared<Test::Testbed2::MQTT::NestedStruct3InterfaceService>(implNestedStruct3Interface, mqttservice);
+
+    mqttservice->connectToHost("");
+    mqttclient->connectToHost("");
+
+    std::condition_variable m_wait;
+    std::mutex m_waitMutex;
+    std::unique_lock<std::mutex> lock(m_waitMutex, std::defer_lock);
+
+
+    std::atomic<bool> is_serviceConnected{ false };
+    auto service_connected_id = serviceNestedStruct3Interface->_subscribeForIsReady([&is_serviceConnected, &m_wait](auto connected)
+        {
+            if (connected)
+            {
+                is_serviceConnected = true;
+                m_wait.notify_all();
+            }
+        });
+    if (serviceNestedStruct3Interface->_is_ready() == true)
+    {
+        is_serviceConnected = true;
+        m_wait.notify_all();
+    }
+    lock.lock();
+    m_wait.wait_for(lock, std::chrono::milliseconds(timeout), [&is_serviceConnected]() { return is_serviceConnected == true; });
+    lock.unlock();
+    REQUIRE(is_serviceConnected);
+ 
+    std::atomic<bool> is_clientConnected{ false };
+    clientNestedStruct3Interface->_subscribeForIsReady([&is_clientConnected, &m_wait](auto connected)
+        {
+            if (connected)
+            {
+                is_clientConnected = true;
+                m_wait.notify_all();
+            }
+        });
+
+    lock.lock();
+    m_wait.wait_for(lock, std::chrono::milliseconds(timeout), [&is_clientConnected]() {return is_clientConnected  == true; });
+    lock.unlock();
+    REQUIRE(is_clientConnected);
+    SECTION("Test setting prop1")
+    {
+        std::atomic<bool> isprop1Changed = false;
+        clientNestedStruct3Interface->_getPublisher().subscribeToProp1Changed(
+        [&isprop1Changed, &m_wait ](auto value){
+            isprop1Changed  = true;
+            m_wait.notify_all();
+        });
+        auto test_value = Testbed2::NestedStruct1();
+        Testbed2::fillTestNestedStruct1(test_value);
+        clientNestedStruct3Interface->setProp1(test_value);;
+        lock.lock();
+        REQUIRE( m_wait.wait_for(lock, std::chrono::milliseconds(timeout), [&isprop1Changed]() {return isprop1Changed  == true; }));
+        lock.unlock();
+        REQUIRE(implNestedStruct3Interface->getProp1() == test_value);
+        REQUIRE(clientNestedStruct3Interface->getProp1() == test_value);
+    }
+    SECTION("Test setting prop2")
+    {
+        std::atomic<bool> isprop2Changed = false;
+        clientNestedStruct3Interface->_getPublisher().subscribeToProp2Changed(
+        [&isprop2Changed, &m_wait ](auto value){
+            isprop2Changed  = true;
+            m_wait.notify_all();
+        });
+        auto test_value = Testbed2::NestedStruct2();
+        Testbed2::fillTestNestedStruct2(test_value);
+        clientNestedStruct3Interface->setProp2(test_value);;
+        lock.lock();
+        REQUIRE( m_wait.wait_for(lock, std::chrono::milliseconds(timeout), [&isprop2Changed]() {return isprop2Changed  == true; }));
+        lock.unlock();
+        REQUIRE(implNestedStruct3Interface->getProp2() == test_value);
+        REQUIRE(clientNestedStruct3Interface->getProp2() == test_value);
+    }
+    SECTION("Test setting prop3")
+    {
+        std::atomic<bool> isprop3Changed = false;
+        clientNestedStruct3Interface->_getPublisher().subscribeToProp3Changed(
+        [&isprop3Changed, &m_wait ](auto value){
+            isprop3Changed  = true;
+            m_wait.notify_all();
+        });
+        auto test_value = Testbed2::NestedStruct3();
+        Testbed2::fillTestNestedStruct3(test_value);
+        clientNestedStruct3Interface->setProp3(test_value);;
+        lock.lock();
+        REQUIRE( m_wait.wait_for(lock, std::chrono::milliseconds(timeout), [&isprop3Changed]() {return isprop3Changed  == true; }));
+        lock.unlock();
+        REQUIRE(implNestedStruct3Interface->getProp3() == test_value);
+        REQUIRE(clientNestedStruct3Interface->getProp3() == test_value);
+    }
+    SECTION("Test emit sig1")
+    {
+        std::atomic<bool> issig1Emitted = false;
+        auto local_param1_struct = Testbed2::NestedStruct1();
+        Testbed2::fillTestNestedStruct1(local_param1_struct);
+
+        clientNestedStruct3Interface->_getPublisher().subscribeToSig1(
+        [&m_wait, &issig1Emitted, &local_param1_struct](const Testbed2::NestedStruct1& param1)
+        {
+            REQUIRE(param1 ==local_param1_struct);
+            issig1Emitted  = true;
+            m_wait.notify_all();
+        });
+
+         implNestedStruct3Interface->_getPublisher().publishSig1(local_param1_struct);
+        lock.lock();
+        REQUIRE( m_wait.wait_for(lock, std::chrono::milliseconds(timeout), [&issig1Emitted ]() {return issig1Emitted   == true; }));
+        lock.unlock();
+    }
+    SECTION("Test emit sig2")
+    {
+        std::atomic<bool> issig2Emitted = false;
+        auto local_param1_struct = Testbed2::NestedStruct1();
+        Testbed2::fillTestNestedStruct1(local_param1_struct);
+        auto local_param2_struct = Testbed2::NestedStruct2();
+        Testbed2::fillTestNestedStruct2(local_param2_struct);
+
+        clientNestedStruct3Interface->_getPublisher().subscribeToSig2(
+        [&m_wait, &issig2Emitted, &local_param1_struct, &local_param2_struct](const Testbed2::NestedStruct1& param1, const Testbed2::NestedStruct2& param2)
+        {
+            REQUIRE(param1 ==local_param1_struct);
+            REQUIRE(param2 ==local_param2_struct);
+            issig2Emitted  = true;
+            m_wait.notify_all();
+        });
+
+         implNestedStruct3Interface->_getPublisher().publishSig2(local_param1_struct, local_param2_struct);
+        lock.lock();
+        REQUIRE( m_wait.wait_for(lock, std::chrono::milliseconds(timeout), [&issig2Emitted ]() {return issig2Emitted   == true; }));
+        lock.unlock();
+    }
+    SECTION("Test emit sig3")
+    {
+        std::atomic<bool> issig3Emitted = false;
+        auto local_param1_struct = Testbed2::NestedStruct1();
+        Testbed2::fillTestNestedStruct1(local_param1_struct);
+        auto local_param2_struct = Testbed2::NestedStruct2();
+        Testbed2::fillTestNestedStruct2(local_param2_struct);
+        auto local_param3_struct = Testbed2::NestedStruct3();
+        Testbed2::fillTestNestedStruct3(local_param3_struct);
+
+        clientNestedStruct3Interface->_getPublisher().subscribeToSig3(
+        [&m_wait, &issig3Emitted, &local_param1_struct, &local_param2_struct, &local_param3_struct](const Testbed2::NestedStruct1& param1, const Testbed2::NestedStruct2& param2, const Testbed2::NestedStruct3& param3)
+        {
+            REQUIRE(param1 ==local_param1_struct);
+            REQUIRE(param2 ==local_param2_struct);
+            REQUIRE(param3 ==local_param3_struct);
+            issig3Emitted  = true;
+            m_wait.notify_all();
+        });
+
+         implNestedStruct3Interface->_getPublisher().publishSig3(local_param1_struct, local_param2_struct, local_param3_struct);
+        lock.lock();
+        REQUIRE( m_wait.wait_for(lock, std::chrono::milliseconds(timeout), [&issig3Emitted ]() {return issig3Emitted   == true; }));
+        lock.unlock();
+    }
+    SECTION("Test method func1")
+    {
+        [[maybe_unused]] auto result =  clientNestedStruct3Interface->func1(Testbed2::NestedStruct1());
+        // CHECK EFFECTS OF YOUR METHOD AFER FUTURE IS DONE
+    }
+    SECTION("Test method func1 async")
+    {
+        std::atomic<bool> finished = false;
+        auto resultFuture = clientNestedStruct3Interface->func1Async(Testbed2::NestedStruct1());
+        auto f = std::async(std::launch::async, [&finished, &resultFuture, &m_wait]() {resultFuture.wait(); finished = true; m_wait.notify_all();});
+        lock.lock();
+        REQUIRE( m_wait.wait_for(lock, std::chrono::milliseconds(timeout), [&finished](){ return finished == true; }));
+        lock.unlock();
+        auto return_value = resultFuture.get();
+        REQUIRE(return_value == Testbed2::NestedStruct1()); 
+        // CHECK EFFECTS OF YOUR METHOD HERE
+    }
+
+    SECTION("Test method func1 async with callback")
+    {
+        std::atomic<bool> finished = false;
+        auto resultFuture = clientNestedStruct3Interface->func1Async(Testbed2::NestedStruct1(),[&finished, &m_wait](NestedStruct1 value){ (void) value; finished = true; m_wait.notify_all(); /* YOU CAN CHECK EFFECTS OF YOUR METHOD HERE */ });
+
+        lock.lock();
+        REQUIRE( m_wait.wait_for(lock, std::chrono::milliseconds(timeout), [&finished](){ return finished == true; }));
+        lock.unlock();
+        auto return_value = resultFuture.get();
+        REQUIRE(return_value == Testbed2::NestedStruct1()); 
+    }
+    SECTION("Test method func2")
+    {
+        [[maybe_unused]] auto result =  clientNestedStruct3Interface->func2(Testbed2::NestedStruct1(), Testbed2::NestedStruct2());
+        // CHECK EFFECTS OF YOUR METHOD AFER FUTURE IS DONE
+    }
+    SECTION("Test method func2 async")
+    {
+        std::atomic<bool> finished = false;
+        auto resultFuture = clientNestedStruct3Interface->func2Async(Testbed2::NestedStruct1(), Testbed2::NestedStruct2());
+        auto f = std::async(std::launch::async, [&finished, &resultFuture, &m_wait]() {resultFuture.wait(); finished = true; m_wait.notify_all();});
+        lock.lock();
+        REQUIRE( m_wait.wait_for(lock, std::chrono::milliseconds(timeout), [&finished](){ return finished == true; }));
+        lock.unlock();
+        auto return_value = resultFuture.get();
+        REQUIRE(return_value == Testbed2::NestedStruct1()); 
+        // CHECK EFFECTS OF YOUR METHOD HERE
+    }
+
+    SECTION("Test method func2 async with callback")
+    {
+        std::atomic<bool> finished = false;
+        auto resultFuture = clientNestedStruct3Interface->func2Async(Testbed2::NestedStruct1(), Testbed2::NestedStruct2(),[&finished, &m_wait](NestedStruct1 value){ (void) value; finished = true; m_wait.notify_all(); /* YOU CAN CHECK EFFECTS OF YOUR METHOD HERE */ });
+
+        lock.lock();
+        REQUIRE( m_wait.wait_for(lock, std::chrono::milliseconds(timeout), [&finished](){ return finished == true; }));
+        lock.unlock();
+        auto return_value = resultFuture.get();
+        REQUIRE(return_value == Testbed2::NestedStruct1()); 
+    }
+    SECTION("Test method func3")
+    {
+        [[maybe_unused]] auto result =  clientNestedStruct3Interface->func3(Testbed2::NestedStruct1(), Testbed2::NestedStruct2(), Testbed2::NestedStruct3());
+        // CHECK EFFECTS OF YOUR METHOD AFER FUTURE IS DONE
+    }
+    SECTION("Test method func3 async")
+    {
+        std::atomic<bool> finished = false;
+        auto resultFuture = clientNestedStruct3Interface->func3Async(Testbed2::NestedStruct1(), Testbed2::NestedStruct2(), Testbed2::NestedStruct3());
+        auto f = std::async(std::launch::async, [&finished, &resultFuture, &m_wait]() {resultFuture.wait(); finished = true; m_wait.notify_all();});
+        lock.lock();
+        REQUIRE( m_wait.wait_for(lock, std::chrono::milliseconds(timeout), [&finished](){ return finished == true; }));
+        lock.unlock();
+        auto return_value = resultFuture.get();
+        REQUIRE(return_value == Testbed2::NestedStruct1()); 
+        // CHECK EFFECTS OF YOUR METHOD HERE
+    }
+
+    SECTION("Test method func3 async with callback")
+    {
+        std::atomic<bool> finished = false;
+        auto resultFuture = clientNestedStruct3Interface->func3Async(Testbed2::NestedStruct1(), Testbed2::NestedStruct2(), Testbed2::NestedStruct3(),[&finished, &m_wait](NestedStruct1 value){ (void) value; finished = true; m_wait.notify_all(); /* YOU CAN CHECK EFFECTS OF YOUR METHOD HERE */ });
+
+        lock.lock();
+        REQUIRE( m_wait.wait_for(lock, std::chrono::milliseconds(timeout), [&finished](){ return finished == true; }));
+        lock.unlock();
+        auto return_value = resultFuture.get();
+        REQUIRE(return_value == Testbed2::NestedStruct1()); 
+    }
+
+    std::atomic<bool> serviceDisconnected{ false };
+    mqttservice->subscribeToConnectionStatus([&serviceDisconnected, &m_wait](auto isConnected) {
+        if (!isConnected)
+        {
+            serviceDisconnected = true;
+            m_wait.notify_all();
+        }
+        
+        });
+
+    mqttservice->disconnect();
+
+    lock.lock();
+    m_wait.wait_for(lock, std::chrono::milliseconds(timeout),
+        [&serviceDisconnected]() { return serviceDisconnected == true; });
+    lock.unlock();
+    REQUIRE(serviceDisconnected);
+
+    std::atomic<bool> clientDisonnected{ false };
+    mqttclient->subscribeToConnectionStatus([&clientDisonnected, &m_wait](auto isConnected) {
+        if (!isConnected)
+        {
+            clientDisonnected = true;
+            m_wait.notify_all();
+        }
+        });
+
+    mqttclient->disconnect();
+
+    lock.lock();
+    m_wait.wait_for(lock, std::chrono::milliseconds(timeout),
+        [&clientDisonnected]() { return clientDisonnected == true; });
+    lock.unlock();
+    REQUIRE(clientDisonnected);
+
+    mqttservice.reset();
+    mqttclient.reset();
+    serviceNestedStruct3Interface.reset();
+    clientNestedStruct3Interface.reset();
+}
