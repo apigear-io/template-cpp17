7f69d24d482c2e6fc2bfd9d4ca417dc47b2499ad
Wolfgang Bremer <wolfgang.bremer@epicgames.com>
2026-04-02 16:39:01 +0200

    fix(olink): use blocking lock in SocketWrapper::onClosed
    Replace try_lock_for with blocking lock to avoid TSan false positive
    with timed_mutex. Blocking is acceptable on the shutdown path.

--- templates/apigear/olink/private/socketwrapper.cpp
+++ templates/apigear/olink/private/socketwrapper.cpp
@@ -125,12 +125,7 @@ void SocketWrapper::close()
 
 void SocketWrapper::onClosed()
 {
-    std::unique_lock<std::timed_mutex> lock(m_socketMutex, std::defer_lock);
-    if (!lock.try_lock_for(std::chrono::milliseconds(100))) {
-        AG_LOG_INFO("Closing socket, some messages may be dropped");
-        m_hasSock = false;
-        return;
-    }
+    std::unique_lock<std::timed_mutex> lock(m_socketMutex);
     m_socket.reset();
     m_hasSock = false;
 }
